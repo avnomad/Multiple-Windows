51ffac9d4493c84c63820cbf69cb59447fc39b69
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2010-11-29 15:40:11 +0200

    more unsuccessful experiments...

--- Multiple Windows/moving window.cpp
+++ Multiple Windows/moving window.cpp
@@ -10,7 +10,7 @@ namespace MovingWindow
 		GetModuleHandle(NULL),	// messageHandler's maching code should be in this .exe file...
 		LoadIcon(NULL,IDI_APPLICATION),
 		LoadCursor(NULL,IDC_ARROW),
-		(HBRUSH)GetStockObject(BLACK_BRUSH),
+		NULL/*(HBRUSH)GetStockObject(BLACK_BRUSH)*/,
 		NULL,
 		esquape(MovingWindow::commonAttributes)
 	};
@@ -19,42 +19,79 @@ namespace MovingWindow
 	{
 		PAINTSTRUCT ps;
 		RECT r;	// for client area
-		static POINT clientAreaPosition;	// in screen physical coordinates
+		static RECT oldClient,newClient,intersection;	// in screen physical coordinates
 
 		switch(message)
 		{
 		case WM_CREATE:
 			++windowCounter;
 			return 0;
-		//case WM_MOVE:
-			//InvalidateRect(window,NULL,FALSE);
-			//ScrollWindow(window,clientAreaPosition.x-(short)LOWORD(argL),clientAreaPosition.y-(short)HIWORD(argL),NULL,NULL);
-			//UpdateWindow(window);
-			//clientAreaPosition.x = (short)LOWORD(argL);
-			//clientAreaPosition.y = (short)HIWORD(argL);
-			//return 0;
-		//case WM_MOVING:				// prevent moving of the window!
-		//	((RECT*)argL)->left = 200;
-		//	((RECT*)argL)->top = 200;
-		//	((RECT*)argL)->right = 600;
-		//	((RECT*)argL)->bottom = 400;
-			//ScrollWindow(window,clientAreaPosition.x-((RECT*)argL)->left,clientAreaPosition.y-((RECT*)argL)->top,NULL,NULL);
-			//UpdateWindow(window);
-			//clientAreaPosition.x = ((RECT*)argL)->left;
-			//clientAreaPosition.y = ((RECT*)argL)->top;
-			//return TRUE;
-		case WM_WINDOWPOSCHANGED:
-			((WINDOWPOS*)argL)->flags |= SWP_NOCOPYBITS;
-			//ScrollWindow(window,clientAreaPosition.x-((WINDOWPOS*)argL)->x,clientAreaPosition.y-((WINDOWPOS*)argL)->y,NULL,NULL);
-			//InvalidateRect(window,NULL,FALSE);
+		case WM_MOVE:
+			GetClientRect(window,&newClient);
+			if(oldClient.right != 0)
+				windowCounter++,windowCounter--;
+			ClientToScreen(window,(POINT*)&newClient.left);
+			ClientToScreen(window,(POINT*)&newClient.right);
+			r.left = (short)LOWORD(argL);
+			r.top = (short)HIWORD(argL);
+			IntersectRect(&intersection,&oldClient,&newClient);
+			ScreenToClient(window,(POINT*)&intersection.left);
+			ScreenToClient(window,(POINT*)&intersection.right);
+			InvalidateRect(window,&newClient,TRUE);
+			ValidateRect(window,&intersection);
 			UpdateWindow(window);
-			clientAreaPosition.x = ((WINDOWPOS*)argL)->x;
-			clientAreaPosition.y = ((WINDOWPOS*)argL)->y;
+			CopyRect(&oldClient,&newClient);
 			return 0;
+		//case WM_WINDOWPOSCHANGED:
+		//	//((WINDOWPOS*)argL)->flags |= SWP_NOCOPYBITS;
+		//	InvalidateRect(window,NULL,FALSE);
+		//	newClient.left = ((WINDOWPOS*)argL)->x;
+		//	newClient.top = ((WINDOWPOS*)argL)->y;
+		//	newClient.right = ((WINDOWPOS*)argL)->x + ((WINDOWPOS*)argL)->cx;
+		//	newClient.bottom = ((WINDOWPOS*)argL)->y + ((WINDOWPOS*)argL)->cy;
+		//	IntersectRect(&intersection,&oldClient,&newClient);
+		//	ScreenToClient(window,(POINT*)&intersection.left);
+		//	ScreenToClient(window,(POINT*)&intersection.right);
+		//	ValidateRect(window,&intersection);
+		//	UpdateWindow(window);
+		//	CopyRect(&oldClient,&newClient);
+		//	return 0;
 		case WM_WINDOWPOSCHANGING:
 			((WINDOWPOS*)argL)->flags |= SWP_NOCOPYBITS;
 			break;
 		case WM_ERASEBKGND:
+			{
+				HDC dc = GetDC(window);
+				GetClientRect(window,&r);
+				FillRect(dc,&r,(HBRUSH)GetStockObject(BLACK_BRUSH));
+				r.left = r.top = 0;
+				ClientToScreen(window,(LPPOINT)&r.left);
+				r.right = GetDeviceCaps(dc,HORZRES);
+				r.bottom = GetDeviceCaps(dc,VERTRES);
+				SetViewportOrgEx(dc,-r.left,-r.top,NULL);
+				// draw lines and curves
+				SelectObject(dc,CreatePen(PS_SOLID,1,RGB(255,255,0)));
+				//SetROP2(dc,R2_XORPEN);
+					LineTo(dc,0,r.bottom-1);
+					LineTo(dc,r.right-1,r.bottom-1);
+					LineTo(dc,r.right-1,0);
+					LineTo(dc,0,0);
+					MoveToEx(dc,0,r.bottom>>1,NULL);
+					LineTo(dc,r.right,r.bottom>>1);
+					MoveToEx(dc,r.right>>1,0,NULL);
+					LineTo(dc,r.right>>1,r.bottom);
+					{
+					POINT controlPoints[] = {{r.right-1,r.bottom>>1},
+												{r.right-1,0},{r.right-1,0},{r.right>>1,0},
+												{0,0},{0,0},{0,r.bottom>>1},
+												{0,r.bottom-1},{0,r.bottom-1},{r.right>>1,r.bottom-1},
+												{r.right-1,r.bottom-1},{r.right-1,r.bottom-1},{r.right-1,r.bottom>>1}
+											};
+					PolyBezier(dc,controlPoints,length(controlPoints));
+					}
+				ReleaseDC(window,dc);
+				ValidateRect(window,NULL);
+			}
 			return 1;
 		case WM_ENTERSIZEMOVE:
 		case WM_EXITSIZEMOVE:
@@ -93,41 +130,41 @@ namespace MovingWindow
 											};
 					PolyBezier(ps.hdc,controlPoints,length(controlPoints));
 					}
-				// draw filled shapes
-				//SetROP2(ps.hdc,R2_COPYPEN);
-				SetBkColor(ps.hdc,RGB(64,64,64));
-				SetMapMode(ps.hdc,MM_ANISOTROPIC);
-				SetViewportExtEx(ps.hdc,r.right,-r.bottom,NULL);
-				SetWindowExtEx(ps.hdc,100,100,NULL);
-				SetViewportOrgEx(ps.hdc,-r.left,r.bottom-1-r.top,NULL);
-				SetWindowOrgEx(ps.hdc,0,0,NULL);
-				SelectObject(ps.hdc,CreateHatchBrush(HS_BDIAGONAL,RGB(192,192,192)));
-				DeleteObject(SelectObject(ps.hdc,GetStockObject(NULL_PEN)));
-					Ellipse(ps.hdc,60,60,90,90);
-				DeleteObject(SelectObject(ps.hdc,CreateHatchBrush(HS_FDIAGONAL,RGB(192,192,192))));
-					Chord(ps.hdc,60,60,90,90,50,60,90,100);
-				DeleteObject(SelectObject(ps.hdc,GetStockObject(NULL_BRUSH)));
-				SelectObject(ps.hdc,CreatePen(PS_DASH,1,RGB(0,32,0)));
-					Ellipse(ps.hdc,60,60,90,90);
-				DeleteObject(SelectObject(ps.hdc,GetStockObject(NULL_PEN)));
-				SetBkColor(ps.hdc,RGB(32,32,32));
-				SelectObject(ps.hdc,CreateHatchBrush(HS_CROSS,RGB(0,128,0)));
-					RoundRect(ps.hdc,5,95,45,55,10,10);
-				DeleteObject(SelectObject(ps.hdc,CreateHatchBrush(HS_HORIZONTAL,RGB(0,128,0))));
-					Pie(ps.hdc,10,90,40,60,40,50,0,75);
-				DeleteObject(SelectObject(ps.hdc,CreateHatchBrush(HS_DIAGCROSS,RGB(0,0,128))));
-					{
-						POINT vertices[] = {{15,10},{35,10},{35,30},{25,35},{15,30},{15,10}};
-						Polygon(ps.hdc,vertices,length(vertices));
-					}
-				DeleteObject(SelectObject(ps.hdc,GetStockObject(DKGRAY_BRUSH)));
-				SelectObject(ps.hdc,GetStockObject(WHITE_PEN));
-					SetRect(&r,55,35,70,25);
-					FillRect(ps.hdc,&r,(HBRUSH)GetStockObject(GRAY_BRUSH));
-					OffsetRect(&r,25,0);
-					FrameRect(ps.hdc,&r,(HBRUSH)GetStockObject(GRAY_BRUSH));	// doesn't seem to work
-					OffsetRect(&r,-15,-5);
-					InvertRect(ps.hdc,&r);
+				//// draw filled shapes
+				////SetROP2(ps.hdc,R2_COPYPEN);
+				//SetBkColor(ps.hdc,RGB(64,64,64));
+				//SetMapMode(ps.hdc,MM_ANISOTROPIC);
+				//SetViewportExtEx(ps.hdc,r.right,-r.bottom,NULL);
+				//SetWindowExtEx(ps.hdc,100,100,NULL);
+				//SetViewportOrgEx(ps.hdc,-r.left,r.bottom-1-r.top,NULL);
+				//SetWindowOrgEx(ps.hdc,0,0,NULL);
+				//SelectObject(ps.hdc,CreateHatchBrush(HS_BDIAGONAL,RGB(192,192,192)));
+				//DeleteObject(SelectObject(ps.hdc,GetStockObject(NULL_PEN)));
+				//	Ellipse(ps.hdc,60,60,90,90);
+				//DeleteObject(SelectObject(ps.hdc,CreateHatchBrush(HS_FDIAGONAL,RGB(192,192,192))));
+				//	Chord(ps.hdc,60,60,90,90,50,60,90,100);
+				//DeleteObject(SelectObject(ps.hdc,GetStockObject(NULL_BRUSH)));
+				//SelectObject(ps.hdc,CreatePen(PS_DASH,1,RGB(0,32,0)));
+				//	Ellipse(ps.hdc,60,60,90,90);
+				//DeleteObject(SelectObject(ps.hdc,GetStockObject(NULL_PEN)));
+				//SetBkColor(ps.hdc,RGB(32,32,32));
+				//SelectObject(ps.hdc,CreateHatchBrush(HS_CROSS,RGB(0,128,0)));
+				//	RoundRect(ps.hdc,5,95,45,55,10,10);
+				//DeleteObject(SelectObject(ps.hdc,CreateHatchBrush(HS_HORIZONTAL,RGB(0,128,0))));
+				//	Pie(ps.hdc,10,90,40,60,40,50,0,75);
+				//DeleteObject(SelectObject(ps.hdc,CreateHatchBrush(HS_DIAGCROSS,RGB(0,0,128))));
+				//	{
+				//		POINT vertices[] = {{15,10},{35,10},{35,30},{25,35},{15,30},{15,10}};
+				//		Polygon(ps.hdc,vertices,length(vertices));
+				//	}
+				//DeleteObject(SelectObject(ps.hdc,GetStockObject(DKGRAY_BRUSH)));
+				//SelectObject(ps.hdc,GetStockObject(WHITE_PEN));
+				//	SetRect(&r,55,35,70,25);
+				//	FillRect(ps.hdc,&r,(HBRUSH)GetStockObject(GRAY_BRUSH));
+				//	OffsetRect(&r,25,0);
+				//	FrameRect(ps.hdc,&r,(HBRUSH)GetStockObject(GRAY_BRUSH));	// doesn't seem to work
+				//	OffsetRect(&r,-15,-5);
+				//	InvertRect(ps.hdc,&r);
 			EndPaint(window,&ps);
 			return 0;
 		case WM_DESTROY:
